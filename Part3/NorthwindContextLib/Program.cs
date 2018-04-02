84ac9eafe05414354971fe23649bde0e0c749b2d
Unknown <aguimar@gmail.com>
2018-04-01 21:18:45 -0300

    Atualizando part3 com o Projeto MVC

---
+++ Part3/NorthwindContextLib/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NorthwindContextLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- Part3/NorthwindWeb/Pages/Suppliers.cshtml.cs
+++ Part3/NorthwindWeb/Pages/Suppliers.cshtml.cs
@@ -1,17 +1,37 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.Generic;
+using System.Linq;
+using Packt.CS7;
+using Microsoft.AspNetCore.Mvc;
 
 namespace NorthwindWeb.Pages
 {
     public class SuppliersModel : PageModel
     {
         public IEnumerable<string> Suppliers { get; set; }
+        [BindProperty]
+        public Supplier Supplier { get; set; }
+
+        private Northwind db;
+        public SuppliersModel(Northwind injectedContext)
+        {
+            db = injectedContext;
+        }
 
         public void OnGet()
         {
             ViewData["Title"] = "Northwind Web Site - Suppliers";
-            Suppliers = new []
-                { "Alpha Co", "Beta Limited", "Gamma Corp" };
+            Suppliers = db.Suppliers.Select(s => s.CompanyName).ToArray();
+        }
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                db.Suppliers.Add(Supplier);
+                db.SaveChanges();
+                return RedirectToPage("/suppliers");
+            }
+            return Page();
         }
     }
 }
